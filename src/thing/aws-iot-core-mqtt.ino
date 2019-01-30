e913ed2c5277250223edcdff5732da5cf45ecf93
bkirilov <bkirilov@shutterfly.com>
2019-01-30 16:03:28 +0200

    Add device arduino code

---
+++ src/thing/aws-iot-core-mqtt.ino
@@ -0,0 +1,88 @@
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>    
+#include <ESP8266AWSIoTMQTTWS.h>
+#include <Adafruit_BME280.h>
+
+const char *ssid = "***";
+const char *password = "***";
+
+char *region = (char *) "***";
+char *endpoint = (char *) "***";
+char *mqttHost = (char *) "***";
+int mqttPort = 443;
+char *iamKeyId = (char *) "***";
+char *iamSecretKey = (char *) "***";
+const char* aws_topic  = "$aws/things/***/shadow/update";
+
+
+ESP8266DateTimeProvider dtp;
+AwsIotSigv4 sigv4(&dtp, region, endpoint, mqttHost, mqttPort, iamKeyId, iamSecretKey);
+AWSConnectionParams cp(sigv4);
+AWSWebSocketClientAdapter adapter(cp);
+AWSMqttClient client(adapter, cp);
+
+float bme_pressure, bme_temp, bme_humidity;
+Adafruit_BME280 bme; // Note Adafruit assumes I2C adress = 0x77 my module (eBay) uses 0x76 so the library address has been changed.
+
+void setup() {
+
+    Serial.begin(115200);
+    while(!Serial) {
+        yield();
+    }
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+
+    pinMode(D3, INPUT_PULLUP); //Set input (SDA) pull-up resistor on
+
+    Wire.setClock(2000000);    // Set I2C bus speed 
+    Wire.begin(D3,D4); // Define which ESP8266 pins to use for SDA, SCL of the Sensor
+    if (!bme.begin()) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      while (1);
+    }
+
+    int res = client.connect();
+    Serial.printf("mqtt connect=%d\n", res);
+
+    if (res == 0) {
+      client.subscribe(aws_topic, 1,
+        [](const char* topic, const char* msg)
+        { Serial.printf("Got msg '%s' on topic %s\n", msg, topic); }
+      );
+    }
+}
+
+void loop() {
+  if (client.isConnected()) {
+    bme_temp     = bme.readTemperature();
+    bme_humidity = bme.readHumidity() + 1.0;     // Plus a correction factor for this sensor
+    bme_pressure = bme.readPressure()/100 + 3.7; // Plus a correction factor for this sensor
+
+    DynamicJsonDocument jsonBuffer;
+    JsonObject root = jsonBuffer.to<JsonObject>();
+    root["clientToken"] = "device1"; 
+    JsonObject state = root.createNestedObject("state");
+    JsonObject state_reported = state.createNestedObject("reported");
+    state_reported["temp"] = bme_temp;
+    state_reported["humidity"] = bme_humidity;
+    state_reported["pressure"] = bme_pressure;
+
+    serializeJson(root, Serial);
+    Serial.println();
+    char shadow[measureJson(root) + 1];
+    serializeJson(root, shadow, sizeof(shadow));
+
+    client.publish(aws_topic, shadow, 0, false);
+    client.yield();
+  } else {
+    Serial.println("Not connected...");
+    delay(2000);
+  }
+
+  delay(30000);
+}
