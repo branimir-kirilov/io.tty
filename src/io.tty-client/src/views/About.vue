379fd86d0e66ae1ba393124f5afb3b0dc209065b
Branimir123 <brani_svetoslavov@abv.bg>
2019-10-03 16:33:11 +0300

    Fixed a nasty bug
    Deleted not needed if
    
    Fixed some styles
    
    Duplicated code

--- src/io.tty-client/src/views/About.vue
+++ src/io.tty-client/src/views/About.vue
@@ -3,20 +3,4 @@
     <a href="https://github.com/Branimir123/io.tty">👉 Check the GitHub repo</a>
   </div>
 </template>
-
-<style scoped>
-.about {
-  margin: 150px auto;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-  font-size: 45px;
-  cursor: pointer;
-}
-
-a:hover {
-  zoom: 1.4;
-}
 </style>
